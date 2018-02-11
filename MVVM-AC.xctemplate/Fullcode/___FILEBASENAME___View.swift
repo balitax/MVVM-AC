8c6127e808c39ed78f18cb82d37cacd4752eefc2
Agus Cahyono <cahyo.mamen@gmail.com>
2018-02-11 22:54:23 +0700

    refactor code

--- MVVM-AC.xctemplate/Fullcode/___FILEBASENAME___View.swift
+++ MVVM-AC.xctemplate/Fullcode/___FILEBASENAME___View.swift
@@ -17,12 +17,6 @@ class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
 
--- MVVM-AC.xctemplate/Storyboard/___FILEBASENAME___View.swift
+++ MVVM-AC.xctemplate/Storyboard/___FILEBASENAME___View.swift
@@ -16,13 +16,7 @@ class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
 
